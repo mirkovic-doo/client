1e4f402dc4f4be665fef45b4769091bb3d4232b6
Dragan Mirkovic <63748840+Dragan2402@users.noreply.github.com>
2024-08-18 19:03:32 +0200

    Feature 4: Added firebase integration (#5)
    Co-authored-by: ivanaste <stevanovic.sw69.2019@uns.ac.rs>

---
+++ src/lib/stores/userStore.ts
@@ -0,0 +1,16 @@
+import { writable } from 'svelte/store';
+
+import type { User } from '$lib/types/user';
+
+const authStore = writable<{
+  isLoggedIn: boolean;
+  user: User | null;
+}>({
+  isLoggedIn: false,
+  user: null,
+});
+
+export default {
+  subscribe: authStore.subscribe,
+  set: authStore.set,
+};
