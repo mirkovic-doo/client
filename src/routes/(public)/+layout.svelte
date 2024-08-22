26cb5d44cd6deae549c3319ef1aafede29352a1c
ivanaste <67589926+ivanaste@users.noreply.github.com>
2024-08-22 18:54:01 +0200

    Feature 6: Login and Signup UI (#7)

---
+++ src/routes/(public)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { goto } from '$app/navigation';
+  import authStore from '$lib/stores/authStore';
+  import userStore from '$lib/stores/userStore';
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    if ($authStore.isLoggedIn && !$userStore) {
+      await goto('/');
+    }
+  });
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
+  import { goto } from '$app/navigation';
   import NetIcon from '$lib/assets/icons/net-icon.svg';
+  import api from '$lib/auth/http';
+  import authStore from '$lib/stores/authStore';
+  import userStore from '$lib/stores/userStore';
+  import { ROUTES } from '$lib/utils/constants';
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    if ($authStore.isLoggedIn) {
+      if ($userStore === null || $userStore === undefined) {
+        const response = await api.userService.getMe();
+        $userStore = response.data;
+      }
+
+      if ($userStore.isGuest) {
+        await goto(ROUTES.GUEST.DASHBOARD);
+      } else {
+        await goto(ROUTES.HOST.DASHBOARD);
+      }
+    } else {
+      goto(ROUTES.LOGIN);
+    }
+  });
 </script>
 
-<div class="relative flex justify-center h-screen bg-gray-100 dark:bg-client-light-black">
+<div class="relative flex justify-center">
   <img src={NetIcon} alt="Net-Icon" width="70%" class="absolute opacity-90 dark:opacity-10 h-full" />
 
   <div class="flex flex-col items-center py-10 w-full overflow-auto md:overflow-hidden">
