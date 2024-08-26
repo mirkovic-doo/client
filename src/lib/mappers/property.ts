3a8aebc144a6c67b580587cb014572f85b2159d7
Dragan Mirkovic <draganmirkovic2402@gmail.com>
2024-08-26 18:50:51 +0200

    Feature 22: Create and update of properties
    Added displaying of properties on host dashboard

--- src/lib/mappers/property.ts
+++ src/lib/mappers/property.ts
@@ -66,3 +66,41 @@ export const mapPropertyResponseToPropertyForm = (response: PropertyResponse): P
     autoConfirmReservation: response.autoConfirmReservation ?? true,
   };
 };
+
+export const mapPropertyResponseToProperty = async (response: PropertyResponse): Promise<Property> => {
+  const imagesUrls = await getImageUrlsFromNames(response.photos || []);
+  return {
+    id: response.id || '',
+    name: response.name || null,
+    location: response.location || '',
+    amenities: response.amenities || [],
+    photos: imagesUrls,
+    minGuests: response.minGuests ?? 0,
+    maxGuests: response.maxGuests ?? 0,
+    pricingOption: response.pricingOption || PricingOption.PerUnit,
+  };
+};
+
+export const mapPropertyFormToPropertyRequest = (form: PropertyForm): PropertyRequest => {
+  return {
+    amenities: form.amenities.split(',').map((amenity) => amenity.trim()),
+    location: form.location,
+    maxGuests: parseInt(form.maxGuests, 10),
+    minGuests: parseInt(form.minGuests, 10),
+    name: form.name,
+    photos: form.photos,
+    pricingOption: form.pricingOption,
+  };
+};
+
+export const mapPropertyResponseToPropertyForm = (response: PropertyResponse): PropertyForm => {
+  return {
+    amenities: response.amenities ? response.amenities.join(', ') : '',
+    location: response.location ?? '',
+    maxGuests: response.maxGuests ? response.maxGuests.toString() : '20',
+    minGuests: response.minGuests ? response.minGuests.toString() : '1',
+    name: response.name || '',
+    photos: response.photos || [],
+    pricingOption: response.pricingOption || PricingOption.PerUnit,
+  };
+};
