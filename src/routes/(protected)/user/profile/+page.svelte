776b12ca37a27f587b68ea9be95314334d436e31
Dragan Mirkovic <63748840+Dragan2402@users.noreply.github.com>
2024-09-03 22:04:52 +0200

    Feature 29: Added deletion of the user (#30)

--- src/routes/(protected)/user/profile/+page.svelte
+++ src/routes/(protected)/user/profile/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { auth } from '$lib/auth/firebase';
   import api from '$lib/auth/http';
   import Button from '$lib/components/common/Button.svelte';
   import Input from '$lib/components/common/Input.svelte';
@@ -11,7 +12,7 @@
   import { updateUserFormConfig } from '$lib/utils/formConfigs';
   import { updateFormSchema } from '$lib/validations/auth';
   import { formatValidationErrors, ValidateSingleFormField } from '$lib/validations/common';
-  import { EmailAuthProvider, reauthenticateWithCredential, updatePassword } from 'firebase/auth';
+  import { EmailAuthProvider, reauthenticateWithCredential, signOut, updatePassword } from 'firebase/auth';
   import { Avatar } from 'flowbite-svelte';
   import { onMount } from 'svelte';
   import toast from 'svelte-french-toast';
@@ -31,6 +32,7 @@
   let validationErrors: object | null = null;
   let isSubmitting = false;
   let isChangePasswordModalOpen = false;
+  let deleteDialogConfirmationOpen = false;
 
   $: errorMessage = (field: string): string | null => {
     return ValidateSingleFormField(validationErrors, field);
@@ -100,13 +102,69 @@
       await updatePassword(userCredentials.user, newPassword);
     }
   };
+
+  const handleDeleteAccount = async () => {
+    if ($userStore?.isGuest) {
+      await deleteGuestUser();
+    } else {
+      await deleteHostUser();
+    }
+    await toast.promise(api.userService.user.deleteUser(), {
+      success: 'Account successfully deleted',
+      loading: 'Deleting account...',
+      error: (e) => e.message,
+    });
+    try {
+      $userStore = null;
+      await signOut(auth);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const deleteGuestUser = async () => {
+    if ($userStore && $userStore.id) {
+      await toast.promise(api.accommodationService.reservation.deleteGuestReservations($userStore.id), {
+        success: 'Guest reservations successfully deleted',
+        loading: 'Deleting guest reservations...',
+        error: (e) => 'You can not delete account while you have active reservations',
+      });
+    }
+  };
+
+  const deleteHostUser = async () => {
+    await toast.promise(api.accommodationService.property.deleteHostProperties(), {
+      success: 'Properties successfully deleted',
+      loading: 'Deleting properties...',
+      error: (e) => 'You can not delete account while you have active reservations',
+    });
+  };
 </script>
 
 <svelte:head>
   <title>Your Profile</title>
 </svelte:head>
 
-<div class="flex justify-center h-full items-center justify-evenly">
+{#if deleteDialogConfirmationOpen}
+  <div class="blur-xl opacity-25 bg-grayscale-800 top-0 absolute w-full h-full z-[98]" />
+
+  <dialog open={true} class="top-[35%] bg-grayscale-800 border-gray-500 border rounded-xl text-white z-[99]">
+    <div class="flex flex-col gap-y-4 items-center p-10">
+      <div>Are you sure you want to delete your account?</div>
+
+      <div class="flex items-center gap-x-4">
+        <button class="px-8 py-2 rounded-xl bg-red-500" on:click={handleDeleteAccount}> Yes </button>
+        <button
+          class="border border-white px-8 py-2 rounded-xl bg-grayscale-800"
+          on:click={() => (deleteDialogConfirmationOpen = false)}
+        >
+          No
+        </button>
+      </div>
+    </div>
+  </dialog>
+{/if}
+<div class="relative flex h-full items-center justify-evenly">
   <div class="text-md flex-col">
     <div class="flex items-center gap-4">
       <Avatar class="!bg-purple-400" size="lg">{$userStore?.firstName[0]}{$userStore?.lastName[0]}</Avatar>
@@ -116,7 +174,18 @@
       </div>
     </div>
     <div class="flex mt-10">
-      <Button color="bg-purple-400" text="Change Password" on:click={() => (isChangePasswordModalOpen = true)} />
+      <Button
+        color="bg-purple-400 w-[20vw]"
+        text="Change Password"
+        on:click={() => (isChangePasswordModalOpen = true)}
+      />
+    </div>
+    <div class="flex mt-10">
+      <Button
+        color="bg-red-500 w-[20vw]"
+        text="Delete Account"
+        on:click={() => (deleteDialogConfirmationOpen = true)}
+      />
     </div>
   </div>
   <form on:submit|preventDefault class="w-[35%] p-8 bg-white rounded-xl flex flex-col gap-2">
